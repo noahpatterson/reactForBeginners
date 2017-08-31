9017d7d905cc94d8d61ee6bc6ff165faef1de171
Noah Patterson <noah@noahpatterson.me>
2017-08-31 06:01:32 -0600

    init + first component

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// let's go!
+import React from 'react';
+// curl brackets return just 1 method
+import { render } from 'react-dom'
+
+//grab the component
+import StorePicker from  './components/StorePicker'
+
+render(<StorePicker />, document.getElementById('main'));
+
