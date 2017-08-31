838ca84d6290983304b06b22a049e8fc4ec0fe5c
Noah Patterson <noah@noahpatterson.me>
2017-08-31 06:31:50 -0600

    git ignore + component in own file + add css

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>hello</p>
+    return(
+      <form className="store-selector">
+        { /* comment */ }
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // let's go!
 import React from 'react';
 // curl brackets return just 1 method
-import { render } from 'react-dom'
+import { render } from 'react-dom';
+
+//css
+import './css/style.css';
 
 //grab the component
 import StorePicker from  './components/StorePicker'
